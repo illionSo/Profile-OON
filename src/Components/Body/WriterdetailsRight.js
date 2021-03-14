2e3799c7deb19604860736ec3e9714bc4d2de96f
Sameer Sam <sameerpokharel29@gmail.com>
2021-03-14 15:36:48 +0545

    bodySection complete

--- src/Components/Body/WriterdetailsRight.js
+++ src/Components/Body/WriterdetailsRight.js
@@ -1,18 +1,18 @@
 export const detailsRight= [
-    {id:'1',icon:'',des:'Professional Contant Writer'},
-    {id:'2',icon:'',des:'Last Degree:Bachelor in Computer Science'},
-    {id:'3',icon:'',des:'OON Level:'}
+    {id:'1',icon:'fas fa-user-edit',des:'Professional Contant Writer'},
+    {id:'2',icon:'fas fa-university',des:'Last Degree:Bachelor in Computer Science'},
+    {id:'3',icon:'far fa-star',des:'OON Level:'}
 ]
 
 const WriterdetailsRight = () => {
     return (
-        <div style={{padding:'3rem'}}>
-            <h3>Writer's Highlight</h3>
+        <div style={{padding:'2rem'}}>
+            <h3 style={{fontWeight:'600'}}>Writer's Highlight</h3>
             <hr></hr>
          {detailsRight.map((detail)=> {
              return (
                  <div style={{display:'flex'}}>
-                 <p>{detail.id}</p>
+                <i className={detail.icon}></i>
                  <h4 style={{marginLeft:'1rem',fontWeight:'600'}}>{detail.des}</h4>
                  </div>
              )
